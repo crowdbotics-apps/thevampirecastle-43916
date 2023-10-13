5b54a35929844b09b99c8be24338a4412f9e8177
Crowdbotics <team@crowdbotics.com>
2023-10-13 09:24:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "thevampirecastle-43916",
+  url: "https://thevampirecastle-43916.botics.co",
+  api: "https://thevampirecastle-43916.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
